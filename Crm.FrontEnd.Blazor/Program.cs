fbdd392719e5f398e5cfbc482dae7e74830f88fe
Hussein Beygi <hussein1997.hb@hotmail.com>
2022-12-09 21:55:25 +0330

    Make Project Multilingual

--- Crm.FrontEnd.Blazor/Program.cs
+++ Crm.FrontEnd.Blazor/Program.cs
@@ -1,6 +1,8 @@
 using Crm.FrontEnd.Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Globalization;
+using Microsoft.JSInterop;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -17,5 +19,38 @@ builder.Services.AddOidcAuthentication(options =>
 	builder.Configuration.Bind("LocalSSO", options.ProviderOptions);
 });
 
+builder.Services.AddSingleton<Leads.Services.LeadsService>();
 
-await builder.Build().RunAsync();
+builder.Services.AddSingleton
+	(current => new System.Net.Http.HttpClient
+	{
+		BaseAddress =
+			new System.Uri(builder.HostEnvironment.BaseAddress),
+	});
+
+
+//await builder.Build().RunAsync();
+
+builder.Services.AddLocalization();
+
+var host = builder.Build();
+
+CultureInfo culture;
+var js = host.Services.GetRequiredService<IJSRuntime>();
+var result = await js.InvokeAsync<string>("blazorCulture.get");
+
+if (result != null)
+{
+	culture = new CultureInfo(result);
+}
+else
+{
+	culture = new CultureInfo("en-US");
+	await js.InvokeVoidAsync("blazorCulture.set", "en-US");
+}
+
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+await host.RunAsync();
+
---
+++ Shared/Framework.Components/Framework.Components/HttpServices/ServiceBase.cs
@@ -0,0 +1,96 @@
+﻿
+using System.Net.Http.Json;
+namespace Framework.HttpServices;
+
+
+public abstract class ServiceBase : object
+{
+	public ServiceBase(System.Net.Http.HttpClient http)
+	//,Client.Services.LogsService logsService) : base()
+	{
+		Http = http;
+		//	LogsService = logsService;
+	}
+
+	protected string BaseUrl { get; set; }
+
+	protected HttpClient Http { get; }
+
+	//protected Client.Services.LogsService LogsService { get; }
+
+	public virtual
+		async
+		System.Threading.Tasks.Task<TResponse>
+		GetAsync<TResponse>(string url, string query = null)
+	{
+		HttpResponseMessage response = null;
+
+		try
+		{
+			string requestUri =
+				$"{BaseUrl}/{url}";
+
+			if (string.IsNullOrWhiteSpace(query) == false)
+			{
+				requestUri =
+					$"{requestUri}?{query}";
+			}
+
+			response =
+				await
+				Http.GetAsync
+				(requestUri: requestUri)
+				;
+
+			response.EnsureSuccessStatusCode();
+
+			if (response.IsSuccessStatusCode)
+			{
+				try
+				{
+					// ReadFromJsonAsync -> Extension Method -> using System.Net.Http.Json;
+					TResponse result =
+						await
+						response?.Content?.ReadFromJsonAsync<TResponse>();
+
+					return result;
+				}
+
+				// When content type is not valid
+				catch (System.NotSupportedException ex)
+				{
+					string errorMessage =
+						$"Exception: {ex.Message} - The content type is not supported.";
+
+					// Static داخل تابع غیر
+					//LogsService.AddLog(type: GetType(), message: errorMessage);
+
+					// Static داخل تابع
+					//LogsService.AddLog(type: typeof(ServiceBase), message: errorMessage);
+				}
+
+				// Invalid JSON
+				catch (System.Text.Json.JsonException ex)
+				{
+					string errorMessage =
+						$"Exception: {ex.Message} - Invalid JSON.";
+
+					//	LogsService.AddLog(type: GetType(), message: errorMessage);
+				}
+			}
+		}
+		catch (System.Net.Http.HttpRequestException ex)
+		{
+			string errorMessage =
+				$"Exception: {ex.Message}";
+
+			//	LogsService.AddLog(type: GetType(), message: errorMessage);
+		}
+		finally
+		{
+			response?.Dispose();
+		}
+
+		return default;
+	}
+}
